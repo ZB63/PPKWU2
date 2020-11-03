0a0c1436badebfd13ad5142e939cfb9f80a54f53
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-11-03 15:04:53 +0100

    Create GET endpoint

--- src/main/java/com/ppkwu/ppkwu/controller/StringController.java
+++ src/main/java/com/ppkwu/ppkwu/controller/StringController.java
@@ -1,8 +1,14 @@
 package com.ppkwu.ppkwu.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringController {
+
+    @GetMapping("check/{stringToCheck}")
+    public String rev() {
+        return "";
+    }
 
 }
