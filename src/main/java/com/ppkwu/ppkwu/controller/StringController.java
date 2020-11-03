e904c5858843f6917b97ceac85fc0238895661a0
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-11-03 15:05:57 +0100

    Change method name to more accurate

--- src/main/java/com/ppkwu/ppkwu/controller/StringController.java
+++ src/main/java/com/ppkwu/ppkwu/controller/StringController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StringController {
 
     @GetMapping("check/{stringToCheck}")
-    public String rev() {
+    public String checkString() {
         return "";
     }
 
