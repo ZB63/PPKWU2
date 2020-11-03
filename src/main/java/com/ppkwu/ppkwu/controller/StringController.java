7b0b78778dd95d52cc5a520064ff67888460bac2
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-11-03 15:12:02 +0100

    Add variable representing passed value

--- src/main/java/com/ppkwu/ppkwu/controller/StringController.java
+++ src/main/java/com/ppkwu/ppkwu/controller/StringController.java
@@ -1,13 +1,14 @@
 package com.ppkwu.ppkwu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringController {
 
     @GetMapping("check/{stringToCheck}")
-    public String checkString() {
+    public String checkString(@PathVariable String stringToCheck) {
         return "";
     }
 
